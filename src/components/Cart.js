267d32eb70ea8c0ac953ee2a691b19ec307d8858
nlukic97 <nlukic97@gmail.com>
2021-11-10 18:08:33 +0100

    cart component also works

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,6 @@
+// Icons
+import { FaShoppingCart as CartIcon } from 'react-icons/fa';
+
 const Cart = ({cart, products, addToCart, removefromCart}) => {
 
     let newCart = cart.filter(item=> (item.quantity !== 0) ? products.find(product => product.id === item.id): false) //get all the items in the cart that are still present in the 'products' state
@@ -13,19 +16,40 @@ const Cart = ({cart, products, addToCart, removefromCart}) => {
 
     return (
         <div>
-            {newProducts.map(cartItem=>{
-                return (
-                    <h1 key={cartItem.id}>
-                        <span>id: {cartItem.id}</span> |
-                        <span>&#x24;{cartItem.price}</span> |
-                        <span> x {cartItem.quantity} </span> |
-                        <span>Total price: &#x24;{cartItem.totalPrice}</span>
-                        <button onClick={()=>{addToCart(cartItem.id, 1)}}>Add item</button>
-                        <button onClick={()=>{removefromCart(cartItem.id, 1)}}>Remove item</button>
-                    </h1>
-                )
-            })}
-            <h1>Sub total: &#x24;{subTotal}</h1>
+            <h1 style={{textAlign:'center'}}>Sub total: &#x24;{subTotal}</h1>
+            <div className="products">
+                {newProducts.map(item=>{
+                    return (
+                        // <h1 key={cartItem.id}>
+                        //     <span>id: {cartItem.id}</span> |
+                        //     <span>&#x24;{cartItem.price}</span> |
+                        //     <span> x {cartItem.quantity} </span> |
+                        //     <span>Total price: &#x24;{cartItem.totalPrice}</span>
+                        //     <button onClick={()=>{addToCart(cartItem.id, 1)}}>Add item</button>
+                        //     <button onClick={()=>{removefromCart(cartItem.id, 1)}}>Remove item</button>
+                        // </h1>
+                        
+                        <div className='product'>
+                            <div>
+                                <div className="img-container">
+                                    <img src={item.imgSrc} alt={item.imgAlt} />
+                                </div>
+                                <div className="product-content">
+                                    <div className="left">
+                                        <h3>{item.title}</h3>
+                                        <h3>x {item.quantity}</h3>
+                                    </div>
+                                    <div className="right">
+                                        <h3>&#x24; {item.price}</h3>
+                                        <button onClick={()=>{addToCart(item.id, 1)}}><span>+</span><CartIcon /></button>
+                                        <button onClick={()=>{removefromCart(item.id, 1)}}>Remove item</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
