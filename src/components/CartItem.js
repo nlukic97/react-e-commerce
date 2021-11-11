cfee2e07dbcfb93579f7009c1973d480e8ffac48
nlukic97 <nlukic97@gmail.com>
2021-11-11 17:42:29 +0100

    saved outline on buttons for adding and removing items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -13,8 +13,8 @@ const CartItem = ({item, addToCart, removefromCart}) => {
             <div>
                 <div style={{textAlign:'center'}}>
                     <div><span>{item.quantity}</span></div>
-                    <button onClick={()=>{addToCart(item.id, 1)}}> <GrAddCircle/></button>
-                    <button onClick={()=>{removefromCart(item.id, 1)}}><AiOutlineMinusCircle/></button>
+                    <button onClick={()=>{addToCart(item.id, 1)}}> <GrAddCircle /></button>
+                    <button onClick={()=>{removefromCart(item.id, 1)}}><AiOutlineMinusCircle /></button>
                 </div>
             </div>
             <h3>&#x24; {item.price * item.quantity}</h3>
