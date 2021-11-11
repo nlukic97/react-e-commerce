faf33839a174ee64706107ada981c30594481705
nlukic97 <nlukic97@gmail.com>
2021-11-11 20:13:15 +0100

    changed shop data

--- src/App.js
+++ src/App.js
@@ -17,52 +17,59 @@ function App() {
   const [products] = useState([
     {
       id:0,
-      title:'Nice cat',
+      title:'Model A',
       price:12,
-      imgSrc:'https://static.wikia.nocookie.net/fa416344-abcc-48e4-a27b-a3083b61d0f0',
-      imgAlt:'cat'
+      imgSrc:'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+      imgAlt:'car'
     },
     {
       id:1,
-      title:'Nice car',
+      title:'Model B',
       price:124,
       imgSrc:'https://www.topgear.com/sites/default/files/images/cars-road-test/2021/11/93e4720f7f50f32e92ee218eccaebbd4/ROW03934.jpg',
-      imgAlt:'cat'
+      imgAlt:'car'
     },
     {
       id:2,
-      title:'Nice car 2',
+      title:'Model C',
       price:124,
       imgSrc:'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F0x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349',
-      imgAlt:'cat'
+      imgAlt:'car'
     },
     {
       id:3,
-      title:'Nice cat',
+      title:'Model D',
       price:12,
-      imgSrc:'https://static.wikia.nocookie.net/fa416344-abcc-48e4-a27b-a3083b61d0f0',
-      imgAlt:'cat'
+      imgSrc:'https://images.unsplash.com/photo-1489824904134-891ab64532f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1631&q=80',
+      imgAlt:'car'
     },
     {
       id:4,
-      title:'Nice cat',
+      title:'Model E',
       price:124,
-      imgSrc:'https://static.wikia.nocookie.net/fa416344-abcc-48e4-a27b-a3083b61d0f0',
-      imgAlt:'cat'
+      imgSrc:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/yugo-gv-placement-1528210669.jpg',
+      imgAlt:'yugo'
     },
     {
       id:5,
-      title:'Nice cat',
+      title:'Model F',
       price:12,
-      imgSrc:'https://static.wikia.nocookie.net/fa416344-abcc-48e4-a27b-a3083b61d0f0',
-      imgAlt:'cat'
+      imgSrc:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Lada_2107_aka_Lada_Riva_October_1995_1452cc.jpg/1200px-Lada_2107_aka_Lada_Riva_October_1995_1452cc.jpg',
+      imgAlt:'lada'
     },
     {
       id:6,
-      title:'Nice cat',
+      title:'Model G',
       price:124,
-      imgSrc:'https://static.wikia.nocookie.net/fa416344-abcc-48e4-a27b-a3083b61d0f0',
-      imgAlt:'cat'
+      imgSrc:'https://images.unsplash.com/photo-1519750292352-c9fc17322ed7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=685&q=80',
+      imgAlt:'scooter'
+    },
+    {
+      id:7,
+      title:'Model H',
+      price:124,
+      imgSrc:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUskD0dRRrP144DXEKF1Tr8dB4oEQjQkfdQ&usqp=CAU',
+      imgAlt:'scooter'
     },
   ])
 
